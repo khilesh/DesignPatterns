c027699da44f50871e919344d4d0cee05d8749a7
Nerdisworking <Nerdisworking@gmail.com>
2022-09-23 19:14:25 -0700

    Base

---
+++ DecoratorPattern/src/main/java/org/traditional/Signal.java
@@ -0,0 +1,13 @@
+package org.traditional;
+
+public class Signal {
+private int magnitude;
+
+public Signal(int theMagnitude) {
+	magnitude = theMagnitude;
+}
+
+public int getMagnitude() {
+	return magnitude;
+}
+}
