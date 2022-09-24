f28ab0ca7fd2817c5eac6c3a9c62c1305875a8fd
Nerdisworking <Nerdisworking@gmail.com>
2022-09-23 19:33:03 -0700

    Base

--- DecoratorPattern/src/main/java/org/traditional/SignalTransformer.java
+++ DecoratorPattern/src/main/java/org/traditional/SignalTransformer.java
@@ -2,15 +2,21 @@ package org.traditional;
 
 public abstract class SignalTransformer {
 
+/*** Attributes **/
 private SignalTransformer next;
 
-public SignalTransformer() {
+/*** Constructors ***/
+public SignalTransformer() { // Initializes the "next" reference to default null
 }
 
 public SignalTransformer(SignalTransformer nextTransformer) {
-	next = nextTransformer;
+	next = nextTransformer; // Initializes the "next" reference to nextTransformer
 }
+
+/*** Behaviour: If the next reference refers to a next transformer, the given signal is transformed using that instance.
+If not, the given signal is returned. ***/
 public Signal transform(Signal signal) {
+	
 	if (next != null) {
 		return next.transform(signal);
 	}
